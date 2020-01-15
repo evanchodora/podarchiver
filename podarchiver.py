d06cf8221b8d839720558f01a4355068c7d9b66e
Evan Chodora <evan.chodora@gmail.com>
2020-01-15 12:36:47 -0500

    Added file downloading function

--- podarchiver.py
+++ podarchiver.py
@@ -1,7 +1,9 @@
+import urllib.request
 import requests
 import bs4
 import os
 import json
+from datetime import datetime
 
 
 # User files
@@ -56,6 +58,89 @@ def initial_setup(xml):
     
     return channel_data['title'], episode_path
     
+def archive_episodes(title, path, xml):
+
+    # Create an array of episode data
+    episodes = xml.find_all('item')
+    
+    # Number of available episodes
+    number = str(len(episodes))
+    
+    print(title + ': ' + 'found ' + number + ' episodes...')
+    
+    # Loop through each episode
+    for i, episode in enumerate(episodes):
+        ep_title = episode.title.string
+        try:
+            ep_num = episode.find('itunes:episode').string
+        except:
+            ep_num = ''
+        try:    
+            ep_link = episode.link.string
+        except:
+            ep_link = ''
+        ep_file = episode.enclosure['url']
+        ep_guid = episode.guid.string
+        
+        ep_date = episode.pubDate.string
+        date_format = '%a, %d %b %Y %H:%M:%S %z'
+        ep_date = datetime.strptime(ep_date, date_format)
+        ep_date = ep_date.strftime('%Y%m%d')
+        
+        ep_image = episode.find('itunes:image')['href']
+        ep_dur = episode.find('itunes:duration').string
+        
+        # Find the best location for this episode summary:
+        ep_summary = episode.find('itunes:summary').string
+        
+        # Store episode data dictionary
+        ep_data = {'title': ep_title,
+                   'num': ep_num,
+                   'link': ep_link,
+                   'file': ep_file,
+                   'guid': ep_guid,
+                   'date': ep_date,
+                   'image': ep_image,
+                   'duration': ep_dur,
+                   'summary': ep_summary
+                   }
+        
+        # Status line
+        status = ' [' + str(i+1) + '/' + number + ']'
+        print('Downloading: ' + title + ' - ' + ep_data['title'] + status)
+        
+        # Build root file name and download cover art and MP3 files
+        root_name = path + '/' + title + '_' + ep['date'] + '_' + ep['title']
+        downloader(title, path, ep_data, root_name)
+        
+        # Write episode data file
+        json_file = root_name + '.data'
+        if not os.path.exists(json_file):
+            with open(json_file, 'w') as file:
+                json.dump(ep_data, file, indent=4)
+        
+    return
+    
+# Function to download the episode MP3 and cover art
+def downloader(title, path, ep, root_name):
+
+    # Cover art
+    image_url = ep['image']
+    image_name = root_name + '.jpg'
+    
+    # MP3 file
+    ep_url = ep['file']
+    ep_name = root_name + '.mp3'
+    
+    # Download the files using urllib
+    opener = urllib.request.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]  # Set user-agent
+    urllib.request.install_opener(opener)
+    urllib.request.urlretrieve(image_url, image_name)  # Download cover art
+    urllib.request.urlretrieve(ep_url, ep_name)  # Download MP3 file
+
+    return
+    
 # Function to ensure series archive directory exists
 def ensure_dir(directory):
     if not os.path.exists(directory):
@@ -63,12 +148,14 @@ def ensure_dir(directory):
     
     return
 
+
 # Read podcast feeds from the file
 feeds = read_feeds(feeds_file)
 
 for feed in feeds:
     xml = get_feedxml(feed)
     title, path = initial_setup(xml)
+    archive_episodes(title, path, xml)
 
 #print(soup.find_all('item'))
 #print(soup)
